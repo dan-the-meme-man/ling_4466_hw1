b4bf42bb3af80c8e06eb7bf6e5f6787bd75cf651
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-01-27 22:43:50 -0500

    Initial commit.

---
+++ encode_bitext.py
@@ -0,0 +1,46 @@
+import spacy
+        
+en_tok = spacy.load('en_core_web_sm')
+sv_tok = spacy.load('sv_core_news_sm')
+        
+# create vocab dictionaries
+en_tok_to_id = {}
+sv_tok_to_id = {}
+
+# read vocab file
+with open('vocab.en', 'r') as f:
+    for line in f:
+        line = line.strip().split()
+        en_tok_to_id[line[1]] = int(line[0])
+        
+with open('vocab.sv', 'r') as f:
+    for line in f:
+        line = line.strip().split()
+        sv_tok_to_id[line[1]] = int(line[0])
+
+# get lines of bitext
+en_lines = open('bitext.en', 'r', encoding='utf-8').readlines()
+sv_lines = open('bitext.sv', 'r', encoding='utf-8').readlines()
+
+num_lines = 0
+
+with open('bitext.encoded', 'w+') as g:
+    for i in range(len(en_lines)):
+        
+        # split lines into tokens
+        en_tokens = [token.text for token in en_tok(en_lines[i].strip())]
+        sv_tokens = [token.text for token in sv_tok(sv_lines[i].strip())]
+        
+        # convert tokens to ids
+        en_ids = [str(en_tok_to_id[token]) for token in en_tokens]
+        sv_ids = [str(sv_tok_to_id[token]) for token in sv_tokens]
+        
+        # write ids to file
+        g.write('1' + '\n')
+        g.write(' '.join(en_ids) + '\n')
+        g.write(' '.join(sv_ids) + '\n')
+            
+        num_lines += 1
+        
+        if num_lines % 100 == 0:
+            print('Processed {} lines'.format(num_lines))
---
+++ make_vocab.py
@@ -0,0 +1,43 @@
+import spacy
+from collections import OrderedDict
+
+for lang, tok_name in zip(('en', 'sv'), ('en_core_web_sm', 'sv_core_news_sm')):
+    
+    try:
+        # load spacy models
+        tok = spacy.load(tok_name)
+    except:
+        spacy.cli.download(tok_name)
+        tok = spacy.load(tok_name)
+    
+    # get lines of bitext
+    lines = open('bitext.' + lang, 'r', encoding='utf-8').readlines()
+    
+    vocab = OrderedDict()
+    
+    tok_to_id = {}
+    
+    num_lines = 0
+    
+    for line in lines:
+        # split line into tokens
+        tokens = [token.text for token in tok(line.strip())]
+        
+        # add tokens to vocab
+        for token in tokens:
+            vocab[token] = vocab.get(token, 0) + 1
+            
+        num_lines += 1
+        
+        if num_lines % 100 == 0:
+            print('Processed {} lines'.format(num_lines))
+            
+    # sort lexicographically
+    vocab = OrderedDict(sorted(vocab.items()))
+            
+    # write vocab to file
+    with open('vocab.' + lang, 'w+') as f:
+        i = 1
+        for key in vocab:
+            f.write(str(i) + ' ' + key + ' ' + str(vocab[key]) + '\n')
+            i += 1

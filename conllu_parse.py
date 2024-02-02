4a01381ce12a43dfea39e93f18361c5f472471b2
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-02-02 01:08:08 -0500

    Updates.

--- conllu_parse.py
+++ conllu_parse.py
@@ -16,33 +16,47 @@ def main():
     models = (pfx + '.A1.15', pfx + '.A3.final')
     errors = dict.fromkeys(models, 0)
 
+    # for each model (1 and 4)
     for model in models:
         
+        # read the GIZA++ output
         model_lines = open(f'giza_out_{model}', 'r', encoding='utf-8').readlines()
         
         with open(f'sv_lines_{model}.conllu', 'w+', encoding='utf-8') as f:        
             
             for i in range(len(en_pos_tagged_data)):
                 
+                # from CoNLL-U tagged data
                 example = en_pos_tagged_data[i]
                 
+                # from GIZA++ output
                 swedish = model_lines[3*i + 1].strip().split()
                 
+                # from GIZA++ output
                 english = model_lines[3*i + 2].strip()
                 
+                # get the indices where tokens should be mapped
                 num_lists = re.findall(token_order_re, english)
                 
+                # process and cast the num lists to integers
                 for j in range(len(num_lists)):
                     num_lists[j] = num_lists[j].split()[1:-1]
                     for k in range(len(num_lists[j])):
                         num_lists[j][k] = int(num_lists[j][k])
                 
+                # remove the token order annotations
                 english = re.sub(token_order_re, '', english)
+                
+                # get english tokens
                 english = english.strip().replace('  ', ' ').split()[1:]
                 
+                # get english POS tags
                 en_pos = [token['upos'] for token in example]
+                
+                # prepare a list of Swedish POS tags
                 sv_pos = [''] * len(swedish)
                 
+                # can check where CoNLL-U and GIZA++ data diverge in number of tokens
                 # if i == 18:
                 #     print(f'English: {len(english)}')
                 #     print(english)
@@ -54,12 +68,14 @@ def main():
                 #     print()
                 #     exit()
                 
+                # try to map every swedish token to an english POS tag
+                # using the mapping of english tokens to POS tags
                 for j in range(len(english)):
                     try:
                         english_pos = en_pos[j]
                     except:
                         errors[model] += 1
-                        break
+                        break # we've exhausted the English POS tags
                     swedish_indices = num_lists[j+1] # +1 because we start with NULL
                     for swedish_index in swedish_indices:
                         sv_pos[swedish_index-1] = english_pos # -1 because aligner starts at 1
@@ -68,10 +84,10 @@ def main():
                 for j in range(len(sv_pos)):
                     if sv_pos[j] == '':
                         sv_pos[j] = 'NOUN'
-                    
+            
+                # TODO: write to file in CoNLL-U format
+            
     print(errors)
-                
-                
-                
+                  
 if __name__ == '__main__':
     main()
